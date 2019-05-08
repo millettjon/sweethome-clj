fd1c7171e882a9e8e5ca138d22f611889ae9976c
Jonathan Millett <jon@millett.net>
2019-05-07 20:05:35 -0400

    Added loft.

--- src/jam/pichilaguna/l_house.clj
+++ src/jam/pichilaguna/l_house.clj
@@ -2,7 +2,8 @@
   (:require [jam.sweethome3d
              :refer [state clean-home add-level add-walls create-room delete-wall
                      invoke-ui midpoint dim dimension-line load-furniture add-door add-furniture]
-             :as sh]))
+             :as sh]
+            [jam.sweethome3d.math-wrap :as m]))
 
 ;; lengths in cm
 (def defaults
@@ -150,19 +151,20 @@
             mud-l        guest-bath-l
             mud-w        guest-bath-w
             deck-w       300
-            garage-w     700
-            master-w     400
+            garage-w     400
+            master-w     500
             master-l     400
-            ]
+            default-h    (-> defaults :level :height)
+            master-h     (+ default-h 50)]
 
         ;; exterior walls
         (add-walls :exterior {:closed? true}
                    [[:x 0 :y 0 :house-E-1]
                     [:x hl :house-S-1]
                     [:y (+ hw master-w) :master-W-1]
-                    [:x #(- % master-l) :master-N-1]
+                    [:x #(- % master-l) :height master-h :master-N-1]
                     [:y #(- % master-w) :house-W-1]
-                    [:x 0 :house-N-1]])
+                    [:x 0 :height default-h :house-N-1]])
         (dimension-line :house-W-1 :house-E-1 {:align :center :from :end})
         (dimension-line :house-N-1 :house-S-1 {:align :outside :from :end})
         (dimension-line :master-W-1 :house-W-1 {:align :center})
@@ -281,76 +283,33 @@
 
 
       ;; ========== LOFT ==========
-;;      (add-level "loft" {:elevation 600})
-
-      #_      (let [peak {:x      1900
-                          :height 450}]
-                (add-walls :exterior {:closed? true}
-                   [[:x 0           :y 0  :height 0   ]
-                    [:x peak              :height peak]
-                    [:x :garage-S-1       :height 0   ]
-                    [               :y hw             ]
-                    [:x peak              :height peak]
-                    [:x 0                 :height 0   ]
-                    ]))
-
-      ;; loft
-#_      (add-walls :interior
-                      ;; loft start x
-                 [[:x 1110 :y 0  :height 10 :loft-N]
-                  [        :y hw                   ]])
-
-      ;; stair
-#_      (add-walls :interior
-                                        ;; loft width
-                 [[:x '(+ (dim :loft-N) 370) :y 0        :stair-N-2]
-                  [                          :y (/ hw 2)           ]])
-
-      ;; closet
-#_      (add-walls :interior
-                 [[:x :stair-N-2 :y hw]
-                            ;; closet width
-                  [:y (- hw 190)]
-                            ;; stair width
-                                ;; closet width
-                  [:x #(+ % 264 230) :closet-S-2]
-                  [:y hw]
-                  ])
-#_      (create-room :around [:closet-S-2 :N] :floor wood-floor)
-
-      ;; loft room ends at stair wall
-#_      (add-walls :virtual
-                 [[:x :stair-N-2 :y (/ hw 2) :loft-temp]
-                  [              :y :closet-S-2]])
-#_      (create-room :around [:loft-temp :N] :floor wood-floor)
-
-
-      ;; bath
-#_      (add-walls :interior
-                                           ;; stair width
-                 [[:x '(+ (dim :stair-N-2) 264) :y 0       :bath-N-2]
-                  [                             :y (/ hw 2)]
-                  [:x :closet-S-2]
-                  [:y 0]])
-#_      (create-room :around [:bath-N-2 :S] :floor bath-floor)
-
-      ;; bedroom
-#_      (add-walls :interior
-                                            ;; bedroom width
-                 [[:x '(+ (dim :closet-S-2) 320) :y 0 :bed-2-S]
-                  [:y hw]])
-
-#_      (create-room :around [:bed-2-S :N] :floor wood-floor)
-#_      (create-room :around [:bed-2-S :S] :floor wood-floor)
-;;      (create-room :around [:loft-temp :S] :floor plywood)
-#_      (delete-wall :loft-temp)
-
+      (add-level "loft" {:elevation 600})
+      (let [peak-x      1700
+            peak-h      325
+            s-wall-h    200
+            front-angle (Math/atan2 peak-h peak-x)
+            loft-start  900
+            loft-N-h    (-> front-angle Math/tan (* loft-start))]
+        (add-walls :exterior {:closed? true}
+                   [[:x 0 :y 0 :height 0 :house-E-2]
+                    [:x peak-x :height peak-h]
+                    [:x :garage-S :height s-wall-h]
+                    [:y :house-W-0]
+                    [:x peak-x :height peak-h]
+                    [:x 0 :height 0]])
+        (add-walls :interior
+                   [[:x loft-start :y 0 :height loft-N-h :loft-N]
+                    [:y hw]])
+        (create-room :around [:loft-N :S] :floor wood-floor :name "loft")
+        (dimension-line :house-N-1 :loft-N {:align :center})
+        (dimension-line :loft-N :garage-S {:align :center :from :end})
+        (dimension-line :house-W-1 :house-E-1 {:align :center :from :end})
+        )
 
       (sh/delete-walls #(= :virtual (:type %)))
-#_      (delete-wall :house-hall-temp)
 
       (sh/set-walls-alpha 0.0)
-#_      (sh/set-level "main")
+      (sh/set-level "main")
       ))
 
   (invoke-ui build))
